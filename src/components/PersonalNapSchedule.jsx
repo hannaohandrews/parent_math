b7452f9c49a4aeff21a571fb95241534472b8d30
Hanna Oh Andrews <63070798+hannaohandrews@users.noreply.github.com>
2025-01-07 10:41:59 -0800

    added buttons

--- src/components/PersonalNapSchedule.jsx
+++ src/components/PersonalNapSchedule.jsx
@@ -89,6 +89,7 @@ export default function PersonalNapSchedule({
 					{timeDifferenceInHours < 0 ? 0 : timeDifferenceInHours} hrs
 				</p>
 			</div>
+			<button>SAVE SCHEDULE</button>
 		</>
 	);
 }
